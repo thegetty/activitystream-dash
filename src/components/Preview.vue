52173f306ebf07883c3810e380cd6249913f556e
benosteen <bosteen@gmail.com>
2019-10-22 08:57:41 -0700

    Adding webpage link to preview, if it exists

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -4,17 +4,24 @@
     <div v-if="visiblePreview">
       <div v-if="jsonData !== undefined" class="card" style="width: auto;">
         <div class="card-header card-title">
-          <h6>{{ this.jsonData._label }} "{{ this.jsonData.type }}" - <a :href="jsonData.id">(item)</a></h6>
+          <h6 v-if="getWebpage(jsonData)"><a :href="getWebpage(jsonData)">{{ jsonData._label }}</a></h6>
+          <h6 v-else>{{ jsonData._label }}</h6>
         </div>
         <div class="card-body">
           <ul class="link-group link-group-flush">
             <li class="list-group-item" v-for="ident in jsonData.identified_by">{{ ident._label }} ({{ident.type}}): {{ident.content}}
-              <ul v-if="ident.classified_as !== undefined">
+<!--               <ul v-if="ident.classified_as !== undefined">
                 <li v-for="classflinks in ident.classified_as"><a :href="classflinks.id">{{ classflinks._label }} </a><em>
                   {{ vocabFlavour(classflinks.id) }}
                 </em></li>
-              </ul>
+              </ul> -->
             </li>
+          </ul>
+          Classified as:
+          <ul v-if="jsonData.classified_as !== undefined">
+            <li v-for="classflinks in jsonData.classified_as"><a :href="classflinks.id">{{ labelOrLink(classflinks) }} </a><em>
+              {{ vocabFlavour(classflinks.id) }}
+            </em></li>
           </ul>
         </div>
       </div>
@@ -68,6 +75,57 @@ import loadingIcon from "../assets/ajax-loader.gif";
           }
         })
         return vocabLabel;
+      },
+      labelOrLink(node) {
+        if (node._label !== undefined && node._label) {
+          return node._label;
+        } else {
+          return node.id;
+        }
+      },
+      getWebpage(node) {
+        var htmlrep = false;
+        var lastPts = 0;
+        var currentBestUrl = "";
+        if (node.subject_of !== undefined) {
+          node.subject_of.forEach((subjofNode) => {
+            var pts = 0
+            if (subjofNode.format === "text/html") {
+              pts += 3;
+            }
+            
+            if (subjofNode.type === "LinguisticObject") {
+              pts += 2;
+            }
+
+            if (subjofNode.classified_as !== undefined) {
+              subjofNode.classified_as.forEach((cfAs) => {
+                if (cfAs._label.toLowerCase() === "web page") {
+                  pts += 1;
+                }
+                if (cfAs._label.toLowerCase() === "primary") {
+                  pts += 2;
+                }
+                if (cfAs.id === "http://vocab.getty.edu/aat/300264578") { // Web page
+                  pts += 1;
+                }
+                if (cfAs.id === "http://vocab.getty.edu/aat/300404670") { // Primary
+                  pts += 3;
+                }
+              })
+            }
+            console.log(subjofNode.id + " ===> " + pts + " pts.");
+            if (pts > lastPts) {
+              currentBestUrl = subjofNode.id;
+              lastPts = pts;
+            }
+          })
+
+          if (lastPts >= 5) {
+            htmlrep = currentBestUrl;
+          }
+        }
+        return htmlrep;
       }
     },
     computed: {
